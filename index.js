dbaa2a2ca5e4dae6f67f0e27c21dca5434804458
Ioan S <ioan.scurtu120@gmail.com>
2019-10-04 14:38:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function dwarfRollCall(dwarves) {
-}
+  var endString = ""
+  for (var i = 0; i < dwarves.length; i++) {
+    endString += (i+1).toString()+ '. ' + dwarves[i] + ' ';
+  };
+  return endString;
+};
 
 function summonCaptainPlanet(planeteerCalls){
 }
